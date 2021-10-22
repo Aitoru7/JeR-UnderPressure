06696c5925c601478084de145371721e85f5a3b3
JesussCG <91668375+JesussCG@users.noreply.github.com>
2021-10-22 21:32:45 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,12 @@
 import Game from './Scenes/Game.js';
 import Mainmenu from './Scenes/Mainmenu.js';
+import Credits from './Scenes/Credits.js';
 
 const config = {
     width: 1600,
     height: 1060,
     type:Phaser.AUTO,
-    scene: [Game, Mainmenu],
+    scene: [Game, Mainmenu, Credits],
     physics: {
         default: 'arcade',
         arcade: {
