0ec929b3c9e87f67c16f12b6e1ca0bfa7ff6cb7d
Marcos <91667857+marcosto29@users.noreply.github.com>
2021-10-24 02:38:26 +0200

    Update Game.js

--- js/Scenes/Game.js
+++ js/Scenes/Game.js
@@ -1,11 +1,18 @@
 import Player from "../Clasess/Player.js";
-
+import Averías from "../Clasess/Averías.js";
+var timer;
 export default class Game extends Phaser.Scene{
     constructor(){
         super({key: 'Game'})
     } 
     init(){
-        this.timer;  
+        this.button1;
+        this.platforms;
+        this.error=5000;
+        this.averias = [];
+        this.averias2= [];
+        this.posi = Math.round(Math.random() * (this.averias.length - 0) + 0);
+        this.timer = this.time.addEvent({callback: () => {this.player2.setAcceleration(0), this.player1.setAcceleration(0)}, delay: 1000, callbackScope: this, loop: true}); 
         this.player1;
         this.player2;
         this.wallpaper;
@@ -14,6 +21,7 @@ export default class Game extends Phaser.Scene{
         this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keyS =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keyD =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.keyE =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
     }
     preload(){
         this.load.spritesheet('robot1', 'Assets/andar-sheet1.png', { frameWidth: 40, frameHeight: 50 });
@@ -21,61 +29,125 @@ export default class Game extends Phaser.Scene{
         this.load.image('stop1', 'Assets/player1.png');
         this.load.image('stop2', 'Assets/player2.png');
         this.load.image('building', 'Assets/Edificio.png');
+        this.load.image('sky', 'Assets/Fondo día.png');
+        this.load.image('interaction', 'Assets/Botón interacción E.png');
+        this.load.image('platform1C', 'Assets/Plataforma Central P1.png');
+        this.load.image('platform1L', 'Assets/Plataforma Lateral P1.png');
+        this.load.image('platform0', 'Assets/Plataforma P0.png');
+        this.load.image('wall1', 'Assets/Pared P1.png');
+        this.load.image('wall0', 'Assets/Pared P0.png');
+        this.load.image('plant', 'Assets/Planta.png');
+        this.load.spritesheet('plantani', 'Assets/Planta-sheet.png', { frameWidth: 60, frameHeight: 120 });
     }
     create(){
-        this.wallpaper = this.physics.add.staticImage(800, 600, 'building');
+        this.platforms = this.physics.add.staticGroup();
+        this.wallpaper = this.physics.add.staticImage(543, 405, 'building').setScale(0.68);
+        /*this.platforms.create(651, 545, 'platform1C').setScale(0.83).refreshBody();
+        this.platforms.create(88, 545, 'platform1L').setScale(0.83).refreshBody();
+        this.platforms.create(141, 635, 'wall0').setScale(2.74).refreshBody();
+        this.platforms.create(1000, 635, 'wall0').setScale(2.74).refreshBody();
+        this.platforms.create(47, 445, 'wall1').setScale(0.63).refreshBody();
+        this.platforms.create(1067, 445, 'wall1').setScale(0.63).refreshBody();
+        this.platforms.create(651, 715, 'platform0').setScale(0.83).refreshBody();*/
         this.player1 = new Player(this, 300, 1000, 'robot1', 'stop1');
         this.player2 = new Player(this, 500, 1000, 'robot2', 'stop2');
-        timer =  this.time.addEvent({callback: () => {this.player2.setAcceleration(0), this.player1.setAcceleration(0)}, delay: 1000, callbackScope: this, loop: true}); 
-        //this.physics.add.collider(this.player1, this.player2, onEvent(this.player1, this.player2));
+        this.averias.push(this.averiaplanta1= new Averías(this, 750, 500,'plantani', 'plant', 0, 4).setScale(0.5));
+        this.averias.push(this.averiaplanta2= new Averías(this,200,1000,'robot1', 'stop1', 0, 2).setScale(0.67).refreshBody());
+        //this.averias.push(this.averiaplanta3= new Averías(this,800,1000,'robot1', 'stop1', 0, 2).setScale(0.67).refreshBody());
+        this.button1 = this.physics.add.staticImage(0, 0, 'interaction').setScale(0.5).setVisible(false).setScale(0.67);
+        this.physics.add.collider(this.player1, this.player2, function onEvent(player1, player2){
+            if(player2.x>player1.x){
+                player2.setAccelerationX(50000);
+                player1.setAccelerationX(-50000);
+            }else{
+                player2.setAccelerationX(-50000);
+                player1.setAccelerationX(50000);
+            }
+            timer;  
+        });
+        this.physics.add.collider(this.player1, this.platforms);
+        this.physics.add.collider(this.player2, this.platforms);
+        for(let i=0; i<this.averias.length; i++){
+            this.physics.add.collider(this.platforms, this.averias[i]);
+        }
+        
     }
-    update(){
+    update(time, delta){
+    this.button1.setVisible(false); 
+    if(time > this.error){
+            Math.round(Math.random() * (this.averias.length-1 - 0) + 0);
+            this.averias[this.posi].anims.play(this.averias[this.posi].key[0], true);
+            this.averias[this.posi].a=true;
+            this.physics.add.collider(this.averias[this.posi], this.player1, function no(){this.player1.velocityX = 0;}, null, this);
+            this.physics.add.collider(this.averias[this.posi], this.player2, function no(){this.player2.velocityX = 0;}, null, this);
+            this.averias[this.posi].setPushable(false);
+            this.averias2.push(this.averias[this.posi]);
+            this.averias.splice(this.averias[this.posi], 1);
+            this.error+=50000000;     
+    }
         if (this.keyA.isDown)
     {
         this.player1.flipX = true;
-        this.player1.setX(this.player1.x-5);
+        this.player1.setVelocityX(-400);
 
         this.player1.anims.play(this.player1.key[0], true);
     }
     else if (this.keyD.isDown)
     {
         this.player1.flipX = false;
-        this.player1.setX(this.player1.x+5);
+        this.player1.setVelocityX(400);
 
         this.player1.anims.play(this.player1.key[0], true);
     }
     else
     {
+        this.player1.setVelocityX(0);
         this.player1.anims.play(this.player1.key[1]);
     }
-    if (this.keyW.isDown) //&& player2.body.touching.down)
+    if (this.keyW.isDown && this.player1.body.touching.down)
     {
-        this.player1.setY(this.player1.y-7);
+        this.player1.setVelocityY(-500);
     }
 
 
     if (this.cursors.left.isDown)
     {
         this.player2.flipX = true;
-        this.player2.setX(this.player2.x-5);
+        this.player2.setVelocityX(-400);
 
         this.player2.anims.play(this.player2.key[0], true);
     }
     else if (this.cursors.right.isDown)
     {
         this.player2.flipX = false;
-        this.player2.setX(this.player2.x+5);
+        this.player2.setVelocityX(400)
 
         this.player2.anims.play(this.player2.key[0], true);
     }
     else
     {
+        this.player2.setVelocityX(0);
         this.player2.anims.play(this.player2.key[1]);
     }
-    if (this.cursors.up.isDown) //&& player2.body.touching.down)
+    if (this.cursors.up.isDown && this.player2.body.touching.down)
     {
-        this.player2.setY(this.player2.y-5);
+        this.player2.setVelocityY(-500);
     }
+    for(let i=0; i<this.averias2.length; i++){
+        if((this.player1.x < this.averias2[i].x+70) && (this.player1.x > this.averias2[i].x-70)){
+            this.button1.x=this.player1.x;
+            this.button1.y=this.player1.y - 50;
+            this.button1.setVisible(true);
+            if(this.keyE.isDown){
+                    console.log("w");
+                    var x = time + 3000;
+                    if(time < x){
+                        console.log("A");
+                        this.player1.setVelocityX(0);
+                    }   
+            }
+        }
+    }
+}
 
-    }
 }
