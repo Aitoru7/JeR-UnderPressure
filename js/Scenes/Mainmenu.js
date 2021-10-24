eee3bffda598bf4545d35dd8bd4021c4c15cc127
JesussCG <91668375+JesussCG@users.noreply.github.com>
2021-10-24 20:48:21 +0200

    Update Mainmenu.js

--- js/Scenes/Mainmenu.js
+++ js/Scenes/Mainmenu.js
@@ -27,31 +27,30 @@ export default class Mainmenu extends Phaser.Scene{
     create(){
         //alert('porfavor funciona2');
         // Game background
-        this.add.image(800, 500, 'sky0');
+        this.add.image(500, 400, 'sky0').setScale(0.8);
         
         //This create the place where the clouds are going to be
-        this.rect = new Phaser.Geom.Rectangle(0, 0, 1600, 1060);
+        this.rect = new Phaser.Geom.Rectangle(0, 0, 1086, 720);
         //Here we inicialite the group of clouds
-        this.group = this.add.group({ key: 'cloud', frameQuantity: 20 });
+        this.group = this.add.group({ key: 'cloud', frameQuantity: 15 });
         //And this initialite the cloud in the exact are
         Phaser.Actions.RandomRectangle(this.group.getChildren(), this.rect);
 
         //Here we create the efect of movement of the backgournd
-        this.back = this.add.tileSprite(0, 0, 1600, 1060, 'back')
-        .setOrigin(0, 0);
-        this.floor = this.add.rectangle(800,1000,1600,400,0x555555);
-        this.front = this.add.tileSprite(0, -50, 1600, 1060, 'front')
-        .setOrigin(0, 0);
+        this.back = this.add.tileSprite(0, -100, 0, 0, 'back')
+        .setOrigin(0, 0).setScale(0.8);
+        this.floor = this.add.rectangle(600,1000,1200, 1000,0x555555);
+        this.front = this.add.tileSprite(0, -100, 1086, 0, 'front')
+        .setOrigin(0, 0).setScale(0.7);
 
         //  The platforms group contains the ground and the 2 ledges we can jump on
         this.platforms = this.physics.add.staticGroup();
 
-        this.title = this.add.image(800,-200, "title").setScale(0.75);
+        this.title = this.add.image(550,-200, "title").setScale(0.5);
 
         //  Here we create the ground.
         //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-        this.platforms.create(600, 1040, 'ground').setScale(3).refreshBody();
-        this.platforms.create(600, 1000, 'ground').setScale(3);
+        this.platforms.create(500, 710, 'ground').refreshBody().setScale(2);
 
         //Configuratión of the animation
         const animConfig = {
@@ -70,8 +69,8 @@ export default class Mainmenu extends Phaser.Scene{
         this.anims.create(animConfig1);
         
         // The player and its settings
-        const sprite = this.add.sprite(600, 910, 'robotA', 'frame_0000').setScale(3);
-        const sprite1 = this.add.sprite(1000, 910, 'robotB', 'frame_0000').setScale(3);
+        const sprite = this.add.sprite(400, 650, 'robotA', 'frame_0000').setScale(2);
+        const sprite1 = this.add.sprite(700, 650, 'robotB', 'frame_0000').setScale(2);
         //player = this.physics.add.sprite(600, 450, 'robotA').setScale(3);
         sprite.play('walk');
         sprite1.play('walk1');
@@ -79,9 +78,9 @@ export default class Mainmenu extends Phaser.Scene{
         sprite.flipX=true;
         sprite1.flipX=true;
 
-        this.backRectangle = this.add.rectangle(-100,-100,250,200,0x000000);
-        this.play=this.add.image(-100,-100,'play').setScale(0.25);
-        this.credits=this.add.image(-100,-100,'credits').setScale(0.25);
+        this.backRectangle = this.add.rectangle(-100,-100,175,150,0x000000);
+        this.play=this.add.image(-100,-100,'play').setScale(0.2);
+        this.credits=this.add.image(-100,-100,'credits').setScale(0.2);
         this.instruciones = this.add.text(-100, -100, '<--  -->', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
     }
 
@@ -95,28 +94,28 @@ export default class Mainmenu extends Phaser.Scene{
         this.back.tilePositionX -= 2;
         this.front.tilePositionX -= 6;
 
-        if(this.title.y < 400){
+        if(this.title.y < 200){
             this.title.y=this.title.y + 3;
         }else if(this.vez==0){
-            this.backRectangle.x=600; this.backRectangle.y=650;
-            this.instruciones.x=575; this.instruciones.y=725;
-            this.play.x=600; this.play.y=650;
-            this.credits.x=1000; this.credits.y=650;
+            this.backRectangle.x=400; this.backRectangle.y=400;
+            this.instruciones.x=370; this.instruciones.y=455;
+            this.play.x=400; this.play.y=400;
+            this.credits.x=700; this.credits.y=400;
             this.vez++;
         }
 
         if (this.cursors.left.isDown){
-            this.backRectangle.x=600; 
-            this.instruciones.x=575;
+            this.backRectangle.x=400; 
+            this.instruciones.x=370;
         }
         if (this.cursors.right.isDown ){
-            this.backRectangle.x=1000;
-            this.instruciones.x=975;
+            this.backRectangle.x=700;
+            this.instruciones.x=670;
         }
 
-        if(this.backRectangle.x==600 && this.keySpace.isDown){
+        if(this.backRectangle.x==400 && this.keySpace.isDown){
             this.scene.start('Game');
-        }else if(this.backRectangle.x==1000 && this.keySpace.isDown){
+        }else if(this.backRectangle.x==700 && this.keySpace.isDown){
             this.scene.start('Credits');
         }
     }
